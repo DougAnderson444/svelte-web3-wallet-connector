0ac2f209997255e1fecea5b86c2bbc3ee64a02ba
Doug A <douganderson444@gmail.com>
2022-08-18 10:55:58 -0300

    rm old reference to Connector

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,2 @@
-import Web3WalletMenu from './Web3WalletMenu.svelte';
-import Web3WalletConnector from './Connector.svelte';
-export { Web3WalletMenu, Web3WalletConnector };
+export { Web3WalletMenu } from './Web3WalletMenu.svelte';
 export default Web3WalletConnector;
